572393b015fe3ff777a171f23bd69e244256f3cb
dubrovsky.K <dubrovsky.kiril@gmail.com>
2019-11-03 17:25:29 +0200

    Complete section 2 and section 3

--- section2/loopingTraingle.js
+++ section2/loopingTraingle.js
@@ -1,3 +1,15 @@
+/* 
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
 let count = 0;
 const maxCount = 6;
 let hashTags = '';
